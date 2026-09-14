0690c9582bfa6a54cb790a96770b18636c63ebca
RevaAlbana <reva.24feb@gmail.com>
2026-09-14 22:32:31 +0700

    Pertemuan 2

--- app/src/main/java/com/pemmob/revaalbana/MainActivity.kt
+++ app/src/main/java/com/pemmob/revaalbana/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,7 +31,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.pemmob.revaalbana.ui.screen.BasicInfoScreen
 import com.pemmob.revaalbana.ui.theme.JualanTheme
+import com.pemmob.revaalbana.ui.theme.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.pemmob.revaalbana.ui.screen.HubungiKamiScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,8 +46,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JualanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LayoutTentangJualan(modifier = Modifier.padding(innerPadding))
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "basic_info") {
+                        composable("basic_info") {
+                            BasicInfoScreen(
+                                onNavigateToContact = {navController.navigate("form_screen")}
+                            )
+                        }
+                        composable("form_screen") {
+                            HubungiKamiScreen(navController = navController)
+                        }
+                    }
                 }
             }
         }
@@ -54,68 +76,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun LayoutTentangJualan(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(all = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .background(Color.Gray),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "Jualan",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                text = "Jualan",
-                color = Color.White,
-                fontWeight = FontWeight.Bold
-            )
-        }
+fun LayoutTentangJualan() {
 
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Text(
-            text = "Tentang Jualan",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Aplikasi Jualan adalah platform yang menadahi produk lokal UMKM di wilayah Kabupaten Purbalingga, Jawa Tengah.",
-            fontSize = 16.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color(0xFFE0E0E0))
-                .padding(16.dp)
-        ) {
-            Text(
-                text = "Misi Kami:",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.weight(1f)
-            )
-            Text(
-                text = "Memajukan UMKM lokal",
-                modifier = Modifier.weight(2f)
-            )
-        }
-    }
 }
 
 @Preview(showBackground = true)
